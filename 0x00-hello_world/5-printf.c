35d72d822c605935050a697825790482cdd8f532
Ivan Elorm Kunneh <ivanekunneh@gmail.com>
2023-02-16 15:43:26 -0800

    using printf in C for the first time

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - to handle the program
+ *
+ * description: handle a basic string using printf
+ *
+ * return: returns zero if program successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
