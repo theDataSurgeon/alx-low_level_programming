023fcdb69f92d6cd4ad3ba7ff62e315f145c682a
Ivan Elorm Kunneh <ivanekunneh@gmail.com>
2023-02-16 15:22:30 -0800

    my first attempt at C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
-/**This is my first attempt at creating comments in C
+/**
+ *This is my first attempt at creating comments in C
  *This is a Betty Style comment structure
- *
- *This short program Write a C program that prints exactly
  * Programming is like building a multilingual puzzle, followed by a new line.
  */
 
@@ -11,5 +10,4 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
