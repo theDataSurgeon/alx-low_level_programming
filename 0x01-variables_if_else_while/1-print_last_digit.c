5f6beb5c8bdd40db226e0d58aac81aceee51f452
Ivan Elorm Kunneh <ivanekunneh@gmail.com>
2023-02-17 10:46:07 -0800

    using conditional statements with %

--- 0x01-variables_if_else_while/1-print_last_digit.c
+++ 0x01-variables_if_else_while/1-print_last_digit.c
@@ -3,29 +3,36 @@
 #include <stdio.h>
 
 /**
- * main - mother function
+ * main - mother of all functions
  *
- * Description: print the value of n status
+ * Description: basic comparisms
  *
  * Return: Always 0 if successful
-*/
-
+ */
 int main(void)
 {
-	int n, digit;
+	int n;
+	int x;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	/*my code*/
-	digit = n % 10; /*gets last digit*/
+	x = n % 10;
+	printf("Last digit of %d is %d ", n, x);
+	if (x > 5)
+	{
+		printf("and is greater than 5");
+	}
+	if (x == 0)
+	{
+		printf("and is 0");
+	}
+	if (x < 6 && x != 0)
+	{
+		printf("and is less than 6 and not 0");
+	}
 
-	if (digit > 5)
-		printf("Last digit of %i is %i and is greater than 5\n", n, digit);
-	else if (digit == 0)
-		printf("Last digit of %i is %i and is 0\n", n, digit);
-	else if (digit < 6 && digit != 0)
-		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, digit);
+	printf("\n");
 
 	return (0);
 }
