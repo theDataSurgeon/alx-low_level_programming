b10fd9d90c155f6418c7a5207bca06786ab019c0
Ivan Elorm Kunneh <ivanekunneh@gmail.com>
2023-02-16 16:32:51 -0800

    first use of sizeof and %lu (unsigned long number)

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - function to handle program
  *
  * Description: this program uses sizeof to print the size of various data types.
  *
@@ -15,10 +15,9 @@ int main(void)
 	/* sizeof checks the size of a variable */
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
-	printf("Size of an double: %lu byte(s)\n", sizeof(double));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	printf("Size of long int: %lu byte(s)\n", sizeof(long int));
+	pirntf("Size of long long int: %lu bytes(s)\n", sizeof(long long int));
+	printf("Size of float: %lu bytes(s)\n", sizeof(float));
 
 	return (0);
 
