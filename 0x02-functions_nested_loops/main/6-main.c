987bfac6cb7814825c71e2ad6237c6bfebb11fd1
Ivan Elorm Kunneh <ivanekunneh@gmail.com>
2023-02-21 13:31:25 -0800

    call print abs value function

---
+++ 0x02-functions_nested_loops/main/6-main.c
@@ -0,0 +1,20 @@
+#include"../main.h"
+
+/**
+ * main - mother function
+ *
+ * Return: 0 if successful
+*/
+
+int main(void)
+{
+	/**
+	 * _abs - render num to its absolute value
+	 *
+	 * @n:     accept input into variable
+	 */
+
+	int _abs(int n);
+
+	return (0);
+}
