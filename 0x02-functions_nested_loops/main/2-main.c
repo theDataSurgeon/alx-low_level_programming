148971c1e2eab7db6c5785a719a1b49ac0ec2791
Ivan Elorm Kunneh <ivanekunneh@gmail.com>
2023-02-21 11:37:47 -0800

    call function void print_alphabet_x10(void)

---
+++ 0x02-functions_nested_loops/main/2-main.c
@@ -0,0 +1,15 @@
+#include"../main.h"
+
+/**
+ * main - mother function
+ *
+ * Description: call function print_alphabet*
+ *
+ * Return: 0 if successful
+*/
+
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
