952dd380f6c160205581ba7344c548cf809fb6cd
Ivan Elorm Kunneh <ivanekunneh@gmail.com>
2023-02-21 14:05:42 -0800

    call summation funtion

---
+++ 0x02-functions_nested_loops/main/10-main.c
@@ -0,0 +1,22 @@
+#include"../main.h"
+
+/**
+ * main - mother function
+ *
+ * Return: 0 if successful
+*/
+
+int main(void)
+{
+	/**
+	 * add - basic summation
+	 *
+	 * @n1:  digit 1
+	 *
+	 * @n2:  digit 2
+	 */
+
+	int add(int n1, int n2);
+
+	return (0);
+}
