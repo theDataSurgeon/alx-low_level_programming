df6b90b77059b2ce93e23a0ce8dbaf3e3096bc08
Ivan Elorm Kunneh <ivanekunneh@gmail.com>
2023-02-21 13:38:19 -0800

    call print last digit function

---
+++ 0x02-functions_nested_loops/main/7-main.c
@@ -0,0 +1,20 @@
+#include"../main.h"
+
+/**
+ * main - mother function
+ *
+ * Return: 0 if successful
+*/
+
+int main(void)
+{
+	/**
+	 * print_last_digit - print last digit of num
+	 *
+	 * @n:                num to be processed
+	 */
+
+	int print_last_digit(int);
+
+	return (0);
+}
