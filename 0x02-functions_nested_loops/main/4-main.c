0cfdb8ab289e31aff409619cacd5e7e186b2c737
Ivan Elorm Kunneh <ivanekunneh@gmail.com>
2023-02-21 12:52:29 -0800

    checks for alphabetic character

---
+++ 0x02-functions_nested_loops/main/4-main.c
@@ -0,0 +1,21 @@
+#include"../main.h"
+
+/**
+ * main - mother function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	/**
+	 * isalpha - checks for alphabetic character
+	 *
+	 * @c:       variable to accept input
+	 *
+	 * Return:   0 if successful
+	 */
+
+	int _isalpha(int c);
+	return (0);
+}
