381d7f62fad4d42f59e8da460f60d099a2d15306
Ivan Elorm Kunneh <ivanekunneh@gmail.com>
2023-02-21 14:11:33 -0800

    call print to 98 function

---
+++ 0x02-functions_nested_loops/main/11-main.c
@@ -0,0 +1,20 @@
+#include"../main.h"
+
+/**
+ * main - mother function
+ *
+ * Return: 0 if successful
+*/
+
+int main(void)
+{
+	/**
+	 * print_to_98 - prnt num from num entered to 98
+	 *
+	 * @n:           accept input to be processed
+	 */
+
+	void print_to_98(int n);
+
+	return (0);
+}
