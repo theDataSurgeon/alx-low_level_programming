a0ef1e1ecafbbe32d765eaf893c9165ac25d1756
Ivan Elorm Kunneh <ivanekunneh@gmail.com>
2023-02-21 13:46:46 -0800

    call the jack_bau* function

---
+++ 0x02-functions_nested_loops/main/8-main.c
@@ -0,0 +1,18 @@
+#include"../main.h"
+
+/**
+ * main - mother function
+ *
+ * Return: 0 if successful
+*/
+
+int main(void)
+{
+	/**
+	 * jack_bauer - does stuff :)
+	 */
+
+	void jack_bauer(void);
+
+	return (0);
+}
