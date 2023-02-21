742350c01307313a4f47304c1ec4b13d79e3a986
Ivan Elorm Kunneh <ivanekunneh@gmail.com>
2023-02-21 11:05:15 -0800

    working with personal stdout

---
+++ 0x02-functions_nested_loops/main/1-main.c
@@ -0,0 +1,15 @@
+#include "../main.h"
+
+/**
+ * main - Mother function
+ *
+ * Description: making my own stdout function
+ *
+ * Return: 0 if successful
+*/
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
