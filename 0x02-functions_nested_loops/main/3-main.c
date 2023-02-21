ec435e3f30ee0356f28bc0fa22b637024428baea
Ivan Elorm Kunneh <ivanekunneh@gmail.com>
2023-02-21 12:31:34 -0800

    checks character case

---
+++ 0x02-functions_nested_loops/main/3-main.c
@@ -0,0 +1,20 @@
+#include"../main.h"
+
+/**
+ * main - mother hen
+ *
+ * Return: 0 if a Success
+*/
+
+int main(void)
+{
+
+	/**
+	 * _islower - checks if upper or lower case
+	 *
+	 * @c: variable to store external input
+	 */
+
+	_islower(int c);
+	return (0);
+}
