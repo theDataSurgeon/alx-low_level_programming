7afabe147d39c04e2402b46ee7065cc4c5b03894
Ivan Elorm Kunneh <ivanekunneh@gmail.com>
2023-02-21 13:59:29 -0800

    call multiples of 9 function

---
+++ 0x02-functions_nested_loops/main/9-main.c
@@ -0,0 +1,18 @@
+#include"../main.h"
+
+/**
+ * main - mother function
+ *
+ * Return: 0 if successful
+*/
+
+int main(void)
+{
+	/**
+	 * times_table - times table (9)
+	 */
+
+	void times_table(void);
+
+	return (0);
+}
