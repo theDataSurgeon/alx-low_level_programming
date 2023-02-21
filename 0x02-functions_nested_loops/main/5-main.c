c84edb265ac79df4469554602ad2d870f817e7f3
Ivan Elorm Kunneh <ivanekunneh@gmail.com>
2023-02-21 13:01:30 -0800

    function that prints the sign of a number

---
+++ 0x02-functions_nested_loops/main/5-main.c
@@ -0,0 +1,21 @@
+#include"../main.h"
+
+/**
+ * main - mother function
+ *
+ * Return: 0 if Successful
+*/
+
+int main(void)
+{
+
+	/**
+	 * print_sign - checks if num is pos or negative
+	 *
+	 * @n:          variable to accept input
+	 */
+
+	int print_sign(int n);
+
+	return (0);
+}
