a538d11a3843370b7986c7b90058b24004e4d7c8
Ivan Elorm Kunneh <ivanekunneh@gmail.com>
2023-02-21 14:33:09 -0800

    advanced stuff

---
+++ 0x02-functions_nested_loops/main/12-main.c
@@ -0,0 +1,20 @@
+#include"../main.h"
+
+/**
+ * main - mother function
+ *
+ * Return: 0 if successful
+*/
+
+int main(void)
+{
+	/**
+	 * print_times_table - the advanced stuff of the prev
+	 *
+	 * @n: accept input into this
+	 */
+
+	void print_times_table(int n);
+
+	return (0);
+}
