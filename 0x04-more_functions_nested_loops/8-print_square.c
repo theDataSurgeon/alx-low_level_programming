7a0c788dfd08d283929fe6f5b2148a62f548b635
Ivan Elorm Kunneh <ivanekunneh@gmail.com>
2023-02-23 12:18:02 -0800

    prit square with #

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,10 +10,16 @@ void print_square(int size)
 {
 	int row, col;
 
-	for (row = 1; row <= size; row++)
+	if (size <= 0)
+		putchar('\n');
+
+	else
 	{
-		for (col = 1; col <= size; col++)
-			_putchar('#');
-		_putchar('\n');
+		for (row = 1; row <= size; row++)
+		{
+			for (col = 1; col <= size; col++)
+				_putchar('#');
+			_putchar('\n');
+		}
 	}
 }
