8f51d8944dd972ffc738a128adaa415b77bb603a
Ivan Elorm Kunneh <ivanekunneh@gmail.com>
2023-02-23 14:14:32 -0800

    first debugging in C

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,17 @@
-#include"main.h"
+#include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
+* main - tests if integer is positive or negative
+*
+* Return: 0 if successful
 */
 
 int main(void)
 {
-        int i;
+	int i;
 
-        i = 0;
+	i = 0;
 	positive_or_negative(i);
 
-        return (0);
+	return (0);
 }
