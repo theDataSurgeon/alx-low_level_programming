6914cf8d545797e27cbcf25f691fb37fdb2a22e3
Ivan Elorm Kunneh <ivanekunneh@gmail.com>
2023-02-23 13:19:54 -0800

    diagonal slash

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -16,7 +16,7 @@ void print_diagonal(int n)
 	{
 		for (postn = 1; postn <= n; ++postn)
 		{
-			for (space = 1; space <= postn; ++space)
+			for (space = 1; space <= postn-1; ++space)
 				_putchar(' ');
 			_putchar(92); /*is equal to '/' char*/
 			_putchar('\n');
