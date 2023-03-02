71b3ea051cf5c482073e76b70762884f79ef698d
Ivan Elorm Kunneh <ivanekunneh@gmail.com>
2023-03-02 10:03:09 -0800

    this gave me headaches upon headaches

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,25 +1,22 @@
 /**
-  * print_number - outputs integer with putchar
-  *
-  * @n: the num to print
-  *
-  * Return: Nothing since void function
-  */
+ * print_number - outputs an integer.
+ *
+ * @n: The integer to be printed.
+ */
+
 void print_number(int n)
 {
-	unsigned int x;
+	unsigned int num = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		num = -num;
 	}
 
-	x = n;
+	if ((num / 10) > 0)
+		print_number(num / 10);
 
-	if (x / 10)
-		print_number(x / 10);
-
-	_putchar(x % 10 + '0');
+	_putchar((num % 10) + '0');
 }
 
