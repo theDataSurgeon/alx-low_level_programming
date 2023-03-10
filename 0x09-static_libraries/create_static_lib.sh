89e7815d16ce814d5d40f7ac10469a0ff9f69c53
Ivan Elorm Kunneh <ivanekunneh@gmail.com>
2023-03-10 11:15:23 -0800

    script to create a static lib in the current dir

---
+++ 0x09-static_libraries/create_static_lib.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#creates a static library called liball.a from
+#all the .c files that are in the current directory
+
+gcc -c -Wall -Werror -Wextra *.c
+ar -rc libmy.a *.o
+ar -rcs libname.a *.o
