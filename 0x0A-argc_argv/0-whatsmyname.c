281280190138804c7a8e22f994f53d6d21ad2a03
Ivan Elorm Kunneh <ivanekunneh@gmail.com>
2023-03-10 14:50:48 -0800

    prints name of program without having to compile again

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,9 +10,10 @@
  * Return: Always 0 (Success)
 */
 
-int main(int __attribute__((unused)) argc, char *argv)
+int main(int __attribute__((unused)) argc, char *argv[])
 {
+	int i = 0;
 
-	printf("%s\n", *argv);
+	printf("%s\n", argv[i]);
 	return (0);
 }
