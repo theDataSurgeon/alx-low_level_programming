6c6d9188c1a0bc77d4ee8bdf28c11788fd267322
Ivan Elorm Kunneh <ivanekunneh@gmail.com>
2023-03-20 14:54:38 -0700

    creating structure called dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - the animal we are using
+ * @name: self described
+ * @age: self described
+ * @owner: - self described
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
