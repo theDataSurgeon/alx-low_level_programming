2218a689b6fa68f1b65a666ebe8bebf610d9ae8b
Ivan Elorm Kunneh <ivanekunneh@gmail.com>
2023-03-20 15:06:00 -0700

    first works*

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,15 @@
+#ifndef DOG
+#define DOG
+
 /**
- * struct dog - the animal we are using
- * @name: self described
- * @age: self described
- * @owner: - self described
-*/
+ * struct dog - dog class
+ *
+ * @name: Name of the dog
+ *
+ * @age: Age of the dog
+ *
+ * @owner: Owner of the dog
+ */
 
 struct dog
 {
@@ -11,3 +17,14 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+#endif
