42ec680ac3e64637d133c5a3dcad780ba904b705
Ivan Elorm Kunneh <ivanekunneh@gmail.com>
2023-04-04 14:51:36 -0700

    first linked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,37 +1,22 @@
 #include "lists.h"
 
 /**
- * print_list - print elements in a linked list
+ * print_listint - prints all elements in list
+ * @h: the head of the list
  *
- * @h: pointer to head first node
- *
- * Return: Number of nodes
-*/
+ * Return: num of nodes present
+ */
 
-size_t print_list(const list_t *h)
+size_t print_listint(const listint_t *h)
 {
-	size_t count_node = 1;
+	size_t count_node = 0;
+	
+	while (h != NULL)
 
-	/*return 0 as number of nodes if null*/
-	if (h == NULL)
-		return (0);
-
-	while (h->next != NULL)
 	{
-		/*if the string is NULL, ouput/return the length as 0 and string as (nil)*/
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
-		else
-			printf("[%d] %s\n", h->len, h->str);
-
-		/*move to the next node in list*/
+		printf("%d\n", h->n);
 		h = h->next;
-		/*count nodes*/
-		count_node += 1;
+		count_node++;
 	}
-
-	/*print for the last node*/
-	printf("[%d] %s\n", h->len, h->str);
-
 	return (count_node);
 }
